1b65ac34147a948f5fb67b7b6f69be3b33514a7f
Angus Brooks <angusbrooks@gmail.com>
2022-03-02 10:59:09 -0500

    add js dir

---
+++ MojoWebMarks/public/js/mojo_common.js
@@ -0,0 +1,119 @@
+var search_submission = 6;	
+
+function setCookie(name,value,days)
+{
+        if (days) {
+                var date = new Date();
+                date.setTime(date.getTime()+(days*24*60*60*1000));
+                var expires = "; expires="+date.toGMTString();
+        }
+        else var expires = "";
+        document.cookie = name+"="+encodeURIComponent(value)+expires+"; path=/";
+}
+
+function getCookie(name,path)
+{
+        var start = document.cookie.indexOf( name + "=" );
+        var len = start + name.length + 1;
+
+        if ( ( !start ) && ( name != document.cookie.substring( 0, name.length ) ) ) {
+                return null;
+        }
+
+        if ( start == -1 ) return null;
+                var end = document.cookie.indexOf( ";", len );
+        if ( end == -1 ) end = document.cookie.length;
+                return decodeURIComponent(document.cookie.substring( len, end ) );
+
+}
+
+
+function eraseCookie(name)
+{
+        setCookie(name,"",-1);
+}
+
+function cgi_out(tab_parm)
+{
+	var sort_asc = 0;
+	var sort_desc = 1;
+	var sort_date_asc = 2;
+	var sort_date_desc = 3;
+	var sort_date_indicator = 11;	
+	var sortCrit; 
+	var counter;
+    var prevTab;
+	var date_flag;
+	var currTab;
+
+	prevTab = parseInt(getCookie('tab'));	
+	counter = parseInt(getCookie('Counter'));	
+	date_flag = parseInt(getCookie('date_flag'));
+
+	currTab = tab_parm.substr(4,2);
+
+	if(currTab == sort_date_indicator && date_flag == 1) {
+	//	alert("A")
+		sortCrit = sort_date_desc
+		eraseCookie("date_flag")
+		tab_parm = "tab=" + prevTab
+		
+	} 
+	else if(currTab == sort_date_indicator) {
+	//	alert("B")
+		sortCrit = sort_date_asc	
+		setCookie('date_flag',1)
+		tab_parm = "tab=" + prevTab
+	} else 
+   	
+	if(currTab != prevTab) {
+	//	alert("C")
+		sortCrit = sort_asc;
+		setCookie('tab', currTab);
+   		setCookie('Counter',1); 
+	}
+	else if (currTab == prevTab) {
+	//	alert("D")
+		sortCrit = (counter++ % 2) ? sort_desc : sort_asc;
+		setCookie('Counter',counter)
+	}
+
+	top.location = "/mojoMarks/tabView?" + tab_parm + "&sortCrit=" + sortCrit;
+}
+
+function setSearchTerms()
+{      
+     var searchBx = parent.top.document.getElementById('searchBx');
+     parent.top.document.getElementById('searchTerms').innerHTML = searchBx.value;
+     setCookie('searchTerms', searchBx.value);
+     setCookie('search_submission', search_submission);
+}
+
+function getSearchTerms()
+{      
+       var searchTerms = getCookie('searchTerms');
+       parent.top.document.getElementById('searchTerms').innerHTML = searchTerms;
+}
+
+function topOpToSearch(topOp)
+{      
+   var searchBox = parent.top.document.getElementById('searchBx');
+   searchBox.value = topOp; 
+}
+
+
+function logOut()
+{
+	eraseCookie("wmSessionID");
+	eraseCookie("wmUserID");
+	eraseCookie("Counter");
+	eraseCookie("dt_cnter");
+	eraseCookie("tab_state");
+	eraseCookie("searchTerms");
+	eraseCookie("search_submission");
+	
+	// vendor specific general Mojo cookie
+	eraseCookie("mojolicious");
+	// vendor specific general Mojo cookie	
+	top.location = "/mojoMarks/logout";
+}
